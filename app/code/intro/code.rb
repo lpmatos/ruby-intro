f4c7e9b0c282cf5496ee55acc8995579a3fc7563
luccapsm <lpmatos@stefanini.com>
2019-12-19 01:37:07 -0300

    Adding new scripts and some comments in README

--- app/code/intro/code.rb
+++ app/code/intro/code.rb
@@ -4,41 +4,49 @@ puts "Opa! Seja bem bindo ao jogo da adivinhação."
 
 puts "Qual o seu nome?"
 
-nome = gets
+nome = gets.strip
 
-puts
-puts
+puts "\n\n"
 
 puts "Ae " + nome
 
-puts
+puts "\n\n"
 
 puts "Sorteando um valor secreto entre 0 - 200..."
 
 secreto = 98
 
-puts
+puts "\n\n"
 
 puts "O valor secreto foi escolhido. Que tal adivinhar qual ele é?"
 
-puts
+puts "\n\n"
 
 puts "Tentativa 1"
 
 puts "Entre com o valor: "
 
-valor = gets
+valor = gets.strip
 
 puts "O valor escolhido foi: " + valor
 
-if valor.to_i == secreto
+# O == é o operador de comparação e o = é o operador de atribuição
+
+# Quando lemos algo vindo do terminal, naturalmente esse valor vem como uma string.
+
+# Para converter um valor para inteiro podemos utilizar a função to_i.
+
+resultado = valor.to_i == secreto
+
+if resultado
     puts "Ae você acertou!!"
 else
     puts "Infelizente você errou..."
 end
 
-
 =begin
+
+Funções de conversão.
 
 to_i will convert the String to an Integer.
 to_f will convert the String to an Float, a floating pont
