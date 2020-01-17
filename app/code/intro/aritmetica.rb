363df5c7e3cd43f9ad76587800e39f5a1dabfccf
lpmatos <lpmatos@stefanini.com>
2020-01-17 12:58:45 -0300

    Adding new intro ruby script - aritmetica

---
+++ app/code/intro/aritmetica.rb
@@ -0,0 +1,20 @@
+#!usr/bin/ruby
+
+if __FILE__ == $0
+
+    # Ruby uses the standard arithmetic operators:
+
+    puts "#{1 + 2}"
+    puts "#{2 - 10}"
+    puts "#{3 * 12}"
+    puts "#{100 / 5}"
+    puts "#{10.0 / 4.0}"
+    puts "#{4 % 3}"
+    puts "#{2 ** 5}"
+
+    puts "#{2.(+50)}"
+    puts "#{20.*(5)}"
+
+    # A classe Integer possui algumas funções relacionadas a número inteiros, enquanto o módulo Math contém funções trigonométricas e outras
+
+end
