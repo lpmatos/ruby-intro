2c81ece0c0dea9bf5a0ff688496afce3400da841
lpmatos <lpmatos@stefanini.com>
2020-01-17 17:01:32 -0300

    Adding loop script ruby

--- app/code/intro/loops.rb
+++ app/code/intro/loops.rb
@@ -1,13 +1,25 @@
-puts "\nLoops\n"
-
+puts "Loops:\n"
 for elemento in 1..5
     puts "Valor - #{elemento}"
 end
 
-puts "\nWhile\n"
-
+puts "\nWhile:\n"
 contador = 10
 while contador != 20
     puts "Iteration - #{contador}"
     contador += 1
+end
+
+puts "\nEach:\n"
+(0..10).each do |value|
+    puts "Iteration - #{value}"
+end
+
+puts "\nEach Only line\n"
+(1..5).each { |value| puts "Iteration - #{value}" }
+
+hash = {nome: "Lucca", idade: 20}
+
+hash.each do |key, valeu|
+    puts "#{key} os #{valeu}"
 end
