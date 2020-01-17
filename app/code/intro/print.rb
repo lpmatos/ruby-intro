75aea543b70044654e04ced0972b5022824a5f69
lpmatos <lpmatos@stefanini.com>
2020-01-17 14:35:23 -0300

    Adding print ruby script

---
+++ app/code/intro/print.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+if __FILE__ == $0
+    value = "Lucca"
+    other = 'Lucca'
+
+    puts value
+    puts other
+    puts value == other
+
+    controll = "Lucca Pessoa"
+
+    puts "Eu sou o #{controll}"
+
+    nome = "Lucca"
+    sobrenome = "Pessoa"
+
+    puts nome + sobrenome
+
+    puts nome + " tem #{20.to_s} anos"
+end
