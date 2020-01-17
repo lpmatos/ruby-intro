ce0e74a08f4de8e5704ae67363c1806ac0a7a87a
lpmatos <lpmatos@stefanini.com>
2020-01-17 13:08:49 -0300

    Adding ruby script - variables

---
+++ app/code/intro/variables.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+
+if __FILE__ == $0
+    valor = 25
+
+    puts valor
+
+    nome = sobrenome = "Lucca"
+
+    puts nome
+
+    puts sobrenome
+
+    valor, idade = 30, 20
+
+    puts valor
+
+    puts idade
+end
