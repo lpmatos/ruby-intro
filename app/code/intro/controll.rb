6a73db5b13f7a8293333caa05335931583f3f50a
lpmatos <lpmatos@stefanini.com>
2020-01-17 16:15:34 -0300

    Adding if statement in ruby

--- app/code/intro/controll.rb
+++ app/code/intro/controll.rb
@@ -1,19 +1,24 @@
 #!/usr/bin/ruby
 
-def if_statement()
-    if value > 0
-        if value.even?
-            puts "Par"
-        else
-            puts "Impar"
-            if value >= 0 and value <= 5
-                puts "Entre 0 - 5"
-            elsif value > 5 and value <= 50
-                puts "Entre 5 - 60"
+def if_statement(valor)
+    if valor.kind_of? Integer
+        if value > 0
+            if value.even?
+                puts "Par"
             else
-                puts "Maior que 50"
+                puts "Impar"
+                if value >= 0 and value <= 5
+                    puts "Entre 0 - 5"
+                elsif value > 5 and value <= 50
+                    puts "Entre 5 - 60"
+                else
+                    puts "Maior que 50"
+                end
             end
         end
+    else
+        puts "Saindo..."
+        exit
     end
 end
 
