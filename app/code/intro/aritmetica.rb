5a4ee9797e81927193295044edfaf3a506711de2
lpmatos <lpmatos@stefanini.com>
2020-01-17 12:59:18 -0300

    Adding new intro ruby script - aritmetica

--- app/code/intro/aritmetica.rb
+++ app/code/intro/aritmetica.rb
@@ -12,7 +12,7 @@ if __FILE__ == $0
     puts "#{4 % 3}"
     puts "#{2 ** 5}"
 
-    puts "#{2.(+50)}"
+    puts "#{2.+(50)}"
     puts "#{20.*(5)}"
 
     # A classe Integer possui algumas funções relacionadas a número inteiros, enquanto o módulo Math contém funções trigonométricas e outras
