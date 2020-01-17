4716a77e3d776a5c0e114e5f1d8caa96f04c7829
lpmatos <lpmatos@stefanini.com>
2020-01-17 15:43:54 -0300

    Adding collections script ruby

---
+++ app/code/intro/collections.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+if __FILE__ == $0
+
+    # ARRAY
+
+    array = [1, 2, 3, 4]
+    puts "#{array}"
+    other = [1, "hello", true]
+    puts "#{other}"
+    puts array[0]
+    puts array[3]
+    puts array[5]
+    puts array[-1]
+    puts array[1, 3]
+    puts array[1..3]
+    puts array.[] 0
+    puts array.[] 5
+    puts array.length
+    puts array.first
+    puts array.last
+
+    puts array.include?("Lucca")
+    puts array.include?(2)
+
+    # HASH
+
+    hash = {"color" => "blue", "number" => 20}
+    puts hash.keys
+    puts hash.values 
+    puts hash["color"]
+    puts hash["value"]
+
+    novo = { nome: "lucca", idade: 20}
+    puts novo.keys
+    puts novo.values 
+end
