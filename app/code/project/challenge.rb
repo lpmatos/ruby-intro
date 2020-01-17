8e0581d475a44377e64b7bd9f1968b7dd5519891
lpmatos <lpmatos@stefanini.com>
2020-01-17 13:20:03 -0300

    Adding script desafio ruby

--- app/code/project/challenge.rb
+++ app/code/project/challenge.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/ruby
 
 def do_stuff(first, last)
-    if first.kind_of? Integer && last.kind_of? Integer
-        calculo, check_one, check_two = first ** last, 2 * (last ** last), (first * last) ** 2
-        if calculo >= check_one && calculo >= check_two
+    if first.kind_of? Integer and last.kind_of? Integer
+        calculo = first ** last
+        check_one = 2 * (last ** last)
+        check_two = (first * last) ** 2
+        if calculo >= check_one and calculo >= check_two
             return True
         else
             return False
@@ -14,7 +16,7 @@ end
 if __FILE__ == $0
     quantidade = (gets.strip).to_i + 1
     for elemento in 0..quantidade
-        first, last = ((gets.strip).split).map {|value| value.to_i}
+        first, last = ((gets.strip).split).map(&:to_i)
         puts first
         puts last
         puts do_stuff(first, last)
