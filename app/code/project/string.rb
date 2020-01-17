08790b53ea05c972e183f71199bca079690f01dd
lpmatos <lpmatos@stefanini.com>
2020-01-17 14:43:23 -0300

    Adding string challenger in ruby

--- app/code/project/string.rb
+++ app/code/project/string.rb
@@ -8,7 +8,7 @@ end
 
 if __FILE__ == $0
     quantidade = (gets.strip).to_i
-    for elemento in 0..quantidade
+    for elemento in 1..quantidade
         nome = (gets.strip).to_s
         function(nome)
     end
