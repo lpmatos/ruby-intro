bb14d6d28759fb3d4d05b0a5a57a925027bee671
lpmatos <lpmatos@stefanini.com>
2020-01-17 16:41:33 -0300

    Adding case script in ruby

---
+++ app/code/intro/case.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+def case_one(valeu)
+    if value.kind_of? Integer
+        if value > 0 and value <= 100
+            case value
+                when 0..30
+                    puts "0-29"
+                when 30..60
+                    puts "30-59"
+                when 60..80
+                    puts "60-79"
+                else
+                    puts "80-100" 
+        else
+            puts "Saindo..."
+            exit
+        end
+    else 
+        puts "Saindo..."
+        exit
+    end
+end
+
+if __FILE__ == $0
+    puts "Digite um valor"
+    valor = (gets.strip).to_i
+    case_one(valor)
+end
