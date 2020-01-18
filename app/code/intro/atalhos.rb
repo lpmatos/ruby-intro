d580c64b9f87779cc6b295f78f350031689f0e21
lpmatos <lpmatos@stefanini.com>
2020-01-18 12:13:23 -0300

    Adding ruby atalhos script

---
+++ app/code/intro/atalhos.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+if __FILE__ == $0
+    a, b, c = 1, 2, 3
+    puts a, b, c
+    a += 1
+    puts a
+
+    valor = gets.strip
+
+    if valor == "lucca"
+        person_name = "Pessoa"
+        other = "Value"
+    end
+
+    name = person_name || "Anonymous"
+
+    puts name
+
+    other ||= "Anonymous"
+
+    puts other
+end
