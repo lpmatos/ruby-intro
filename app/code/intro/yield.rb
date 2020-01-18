f9981d9e9f02bac53638997cf8efac64c346410d
lpmatos <lpmatos@stefanini.com>
2020-01-18 11:22:02 -0300

    Adding some comments and ruby yield script

--- app/code/intro/yield.rb
+++ app/code/intro/yield.rb
@@ -4,6 +4,13 @@ def for_each(array)
     end
 end
    
+def show(value)
+    puts "Iremos printar o valor: "
+    yield value
+end
+
 for_each [1,2,3,4,5] do |valor|
     puts "#{valor}"
 end
+
+show puts 10
