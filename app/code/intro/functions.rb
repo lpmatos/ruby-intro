53593c83e73736941df540d4d6c2aa8455318bfb
lpmatos <lpmatos@stefanini.com>
2020-01-18 11:12:00 -0300

    Adding script ruby funtions

--- app/code/intro/functions.rb
+++ app/code/intro/functions.rb
@@ -14,6 +14,20 @@ def other(value)
     end
 end
 
+def surround
+    puts "{"
+    yield
+    puts "}"
+end
+
+def guests(&block)
+    block.call "Something"
+end
+
+def guests(*array)
+    array.each{ |guest| puts "#{guest}" }
+end
+
 if __FILE__ == $0
     puts "Digite um valor inicial:"
     valor = gets.strip.to_i
@@ -21,4 +35,9 @@ if __FILE__ == $0
     puts "#{par valor}"
     puts "#{other valor}"
     puts "#{other other valor}"
+
+    surround { puts "Opa" }
+
+    guests("lucca", 1, 2, 3, "pessoa")
+    
 end
